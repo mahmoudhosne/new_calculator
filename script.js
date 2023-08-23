10db39e78de951e72a5168e52a9cab195e868655
unknown <you@example.comgit config --global user.email mahmoudhossn3@gmail.com>
2023-08-23 17:51:17 +0300

    second update

--- script.js
+++ script.js
@@ -11,6 +11,24 @@ const getDomEle = function (className) {
   return document.querySelector(`.${className}`);
 };
 
+const calcNumbers = function (firstNum, operationS, seconedNum, resultCalc) {
+  if (operationS.textContent === '+') {
+    firstNum = +firstNum + +seconedNum;
+    resultCalc = firstNum;
+  } else if (operationS.textContent === '-') {
+    firstNum = +firstNum + +seconedNum;
+    resultCalc = firstNum;
+  } else if (operationS.textContent === '*') {
+    firstNum = +firstNum * +seconedNum;
+    resultCalc = firstNum;
+  } else if (operationS.textContent === '/') {
+    firstNum = +firstNum / +seconedNum;
+    resultCalc = firstNum;
+  }
+};
+// let x = 0;
+// console.log(10, '+', 5, x);
+
 let firstField = getDomEle('value-input--0');
 let secondField = getDomEle('value-input--2');
 
@@ -29,11 +47,19 @@ const btn7 = getDomEle('btn-7');
 const btn8 = getDomEle('btn-8');
 const btn9 = getDomEle('btn-9');
 btn0.addEventListener('click', function () {
-  if (currentNumber[0].length > 0) {
+  if (firstField.textContent !== '' && operationSignField.textContent === '') {
     firstField.textContent += btn0.textContent;
     currentNumber[0] = firstField.textContent;
-  } else {
-    firstField.textContent = btn0.textContent;
+  } else if (
+    firstField.textContent !== '' &&
+    operationSignField.textContent !== ''
+  ) {
+    if (secondField.textContent > 0) {
+      secondField.textContent += btn0.textContent;
+      currentNumber[2] = secondField.textContent;
+    } else {
+      secondField.textContent = btn0.textContent;
+    }
   }
 });
 const btns = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9];
@@ -45,11 +71,9 @@ for (const btn of btns) {
         currentNumber[3] = '';
         btnResult.textContent = currentNumber[3];
       }
-      activeField = 0;
       firstField.textContent += btn.textContent;
       currentNumber[0] = firstField.textContent;
-    } else if (operationSignField.textContent !== '') {
-      activeField = 2;
+    } else {
       secondField.textContent += btn.textContent;
       currentNumber[2] = secondField.textContent;
     }
@@ -67,6 +91,7 @@ const btnOperationSignsArr = [devide, muiltplye, mines, plus];
 for (const operationSign of btnOperationSignsArr) {
   operationSign.addEventListener('click', function () {
     if (firstField.textContent !== '') {
+      activeField = 2;
       operationSignField.textContent = operationSign.textContent;
     }
   });
@@ -86,6 +111,7 @@ btnEquall.addEventListener('click', function () {
       btnResult.textContent =
         +firstField.textContent + +secondField.textContent;
       firstField.textContent = '';
+      currentNumber[1] = operationSignField.textContent;
       operationSignField.textContent = '';
       secondField.textContent = '';
       currentNumber[3] = btnResult.textContent;
@@ -93,6 +119,7 @@ btnEquall.addEventListener('click', function () {
       btnResult.textContent =
         +firstField.textContent - +secondField.textContent;
       firstField.textContent = '';
+      currentNumber[1] = operationSignField.textContent;
       operationSignField.textContent = '';
       secondField.textContent = '';
       currentNumber[3] = btnResult.textContent;
@@ -100,12 +127,14 @@ btnEquall.addEventListener('click', function () {
       btnResult.textContent =
         +firstField.textContent * +secondField.textContent;
       firstField.textContent = '';
+      currentNumber[1] = operationSignField.textContent;
       operationSignField.textContent = '';
       secondField.textContent = '';
       currentNumber[3] = btnResult.textContent;
     } else if (operationSignField.textContent === '/') {
       btnResult.textContent =
         +firstField.textContent / +secondField.textContent;
+      currentNumber[1] = operationSignField.textContent;
       firstField.textContent = '';
       operationSignField.textContent = '';
       secondField.textContent = '';
@@ -113,9 +142,12 @@ btnEquall.addEventListener('click', function () {
     }
   }
 });
+// get all operations every hour
+
 // // //reset all fields
 
 const btnAc = document.querySelector('.btn-ac');
+
 btnAc.addEventListener('click', function () {
   document.querySelector('.value-input--0').textContent = '';
   document.querySelector('.value-input--2').textContent = '';
@@ -126,6 +158,104 @@ btnAc.addEventListener('click', function () {
   currentNumber = [0, '', 0, 0];
 });
 
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+//   }
+// });
+document.addEventListener('keydown', function (e) {
+  if (e.key >= 0) {
+    if (e.key === '0') {
+      if (
+        firstField.textContent === '' &&
+        operationSignField.textContent === ''
+      ) {
+        firstField.textContent = e.key;
+      } else if (
+        firstField.textContent > 0 &&
+        operationSignField.textContent === ''
+      ) {
+        if (currentNumber[3] > 0) {
+          currentNumber[3] = '';
+          btnResult.textContent = currentNumber[3];
+        }
+        firstField.textContent += e.key;
+        currentNumber[0] = firstField.textContent;
+      } else if (
+        firstField.textContent > 0 &&
+        operationSignField.textContent !== ''
+      ) {
+        if (secondField.textContent === '') {
+          secondField.textContent = e.key;
+        } else if (secondField.textContent > 0) {
+          if (currentNumber[3] > 0) {
+            currentNumber[3] = '';
+            btnResult.textContent = currentNumber[3];
+          }
+          secondField.textContent += e.key;
+          currentNumber[2] = secondField.textContent;
+        }
+      }
+    } else {
+      if (operationSignField.textContent === '') {
+        if (currentNumber[3] > 0) {
+          currentNumber[3] = '';
+          btnResult.textContent = currentNumber[3];
+        }
+        firstField.textContent += e.key;
+      } else {
+        if (currentNumber[3] > 0) {
+          currentNumber[3] = '';
+          btnResult.textContent = currentNumber[3];
+        }
+        secondField.textContent += e.key;
+      }
+    }
+  } else if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+    if (firstField.textContent >= 0) {
+      operationSignField.textContent = e.key;
+    }
+  } else if (e.key === 'Enter') {
+    if (operationSignField.textContent !== '') {
+      if (operationSignField.textContent === '+') {
+        btnResult.textContent =
+          +firstField.textContent + +secondField.textContent;
+        currentNumber[3] = btnResult.textContent;
+        firstField.textContent = '';
+        operationSignField.textContent = '';
+        secondField.textContent = '';
+      } else if (operationSignField.textContent === '-') {
+        btnResult.textContent =
+          +firstField.textContent - +secondField.textContent;
+        currentNumber[3] = btnResult.textContent;
+        firstField.textContent = '';
+        operationSignField.textContent = '';
+        secondField.textContent = '';
+      } else if (operationSignField.textContent === '*') {
+        btnResult.textContent =
+          +firstField.textContent * +secondField.textContent;
+        currentNumber[3] = btnResult.textContent;
+        firstField.textContent = '';
+        operationSignField.textContent = '';
+        secondField.textContent = '';
+      } else if (operationSignField.textContent === '/') {
+        btnResult.textContent =
+          +firstField.textContent / +secondField.textContent;
+        currentNumber[3] = btnResult.textContent;
+        firstField.textContent = '';
+        operationSignField.textContent = '';
+        secondField.textContent = '';
+      }
+    }
+  }
+});
+// document.addEventListener('keydown', function (e) {
+//   // if (e.key >= 0) {
+//   //   document.querySelector(`.value-input--${activeField}`).textContent = e.key;
+//   // }
+//   console.log(e);
+// });
 // close program
 const btnClose = getDomEle('btn-close');
 const content = getDomEle('content');
